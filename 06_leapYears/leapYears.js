0cd0338164f2983aa26d8b605b4495064a0a824a
Theorchero <dumnemtech@gmail.com>
2023-11-19 16:33:54 -0500

    solved!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+// const leapYears = function (number) {
+//   console.log(number);
+//   console.log(number % 4);
+//   if (number % 4 == 0) {
+//     //divided by 4
+//     if (number % 100 == 0) {
+//       if (number % 400 == 0) {
+//         return true; //Divisible by 4, 100, and 400
+//       }
+//     } else {
+//       return false; //Divisible by 100 but not 400
+//     }
+//   } else {
+//     return false; //not divisible by 4
+//   }
+// };
 
+const leapYears = function (number) {
+  if (number % 4 == 0) {
+    if (number % 100 == 0 && number % 400 != 0) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
